e8a51306c59ce331bee0a83be6da7b49a6c4399b
RobinMicek <micek.robin@gmail.com>
2024-05-26 11:10:47 +0200

    POS - Fix gitignore for electron building

---
+++ source/pos/build/main.js
@@ -0,0 +1,35 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('node:path')
+
+function createWindow () {
+  const mainWindow = new BrowserWindow({
+    width: 1600,
+    height: 900,
+    label: "POS",
+    icon: __dirname + "/src/favicon.png",
+    autoHideMenuBar: true
+  })
+
+  mainWindow.loadFile('src/index.html')
+
+  // Open the DevTools.
+  // mainWindow.webContents.openDevTools()
+
+  // Launch in fullscreen
+  // mainWindow.fullScreen = true
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit()
+})
